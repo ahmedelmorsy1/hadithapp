1b1ec84fc85cafb9441a3698875525832d0ecc8b
ahmedelmorsy1 <38796224+ahmedelmorsy1@users.noreply.github.com>
2023-05-24 20:10:31 +0300

    Update home_screen.dart

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -1,44 +1,4 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
-// import 'package:flutter/material.dart';
-// import 'package:get/get.dart';
-// import 'package:hadithapp/controller/hadith_controller.dart';
-// // import 'package:url_launcher/url_launcher.dart';
 
-// class HomeScreen extends StatelessWidget {
-//   const HomeScreen({Key? key}) : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     final HadithController controller = Get.put(HadithController());
-//     return Scaffold(
-//         appBar: AppBar(
-//           title: const Text('Hadith'),
-//           centerTitle: true,
-//         ),
-//         body: Obx(
-//               () =>
-//           controller.isLoading.value
-//               ? const Center(
-//             child: CircularProgressIndicator(),
-//           )
-//               : ListView.builder(
-//               itemCount: controller.hadithModel?.data?.length ?? 0,
-//               itemBuilder: (context, index) {
-//                 return ListTile(
-//                     title: Text(
-//                         controller.hadithModel?.data![index].number.toString() ??
-//                             'no name'),
-//                     subtitle: Text(controller
-//                         .hadithModel?.data![index].arab ??
-//                         'no description'),
-
-//                     onTap: () {
-//                       // _launchInBrowser(Uri.parse(controller.hadithModel?.data![index].id!));
-//                     });
-//               }),
-//         ));
-//   }
-// }
 
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
